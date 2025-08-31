207a777ba439a182e31cf8e268ccb67184588209
Vũ Đức Thắng <113279242+Vuthangvt1405@users.noreply.github.com>
2025-08-31 08:56:11 +0700

    Update EmailListServlet.java

--- src/main/java/com/thang/controllers/EmailListServlet.java
+++ src/main/java/com/thang/controllers/EmailListServlet.java
@@ -16,31 +16,46 @@ public class EmailListServlet extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String firstName = req.getParameter("firstName");
-		String lastName = req.getParameter("lastName");
-		String email = req.getParameter("email");
-		if (firstName == null || firstName.isEmpty() || lastName == null || lastName.isEmpty() || email == null
-				|| email.isEmpty()) {
+		String url = "/index.jsp";
 
-			String message = "Please fill out all three text boxes.";
-			req.setAttribute("message", message);
+//		get action 
+		String action = req.getParameter("action");
 
-			RequestDispatcher rd = req.getRequestDispatcher("index.jsp");
-			rd.forward(req, resp);
-		} else {
-			// Nếu dữ liệu hợp lệ, tạo đối tượng User
-			User user = new User(firstName, lastName, email);
-			req.setAttribute("user", user);
-
-			// Chuyển tiếp đến trang cảm ơn (thanks.jsp)
-			RequestDispatcher rd = req.getRequestDispatcher("thanks.jsp");
-			rd.forward(req, resp);
+		if (action == null) {
+			action = "join";
+			url = "/index.jsp";
 		}
+
+		if (action.equals("add")) {
+			String firstName = req.getParameter("firstName");
+			String lastName = req.getParameter("lastName");
+			String email = req.getParameter("email");
+
+			User user = new User(firstName, lastName, email);
+
+			String message;
+
+			if (firstName == null || lastName == null || email == null || firstName.isEmpty() || lastName.isEmpty()
+					|| email.isEmpty()) {
+
+				message = "Please fill out all three text boxes";
+				url = "/index.jsp";
+				System.out.printf("First Name: %s, Last Name: %s, Email: %s\n", firstName, lastName, email);
+			} else {
+				message = "";
+				url = "/thanks.jsp";
+//				UserDB.insert(user);
+			}
+			req.setAttribute("user", user);
+			req.setAttribute("message", message);
+		}
+		getServletContext().getRequestDispatcher(url).forward(req, resp);
+
 	}
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		doPost(req, resp);
-		
-		}
+
+	}
 }
